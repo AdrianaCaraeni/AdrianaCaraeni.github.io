9f60f094d2c91e6af3c6732a18e42fddc668ed24
Adriana Caraeni <acaraeni10@gmail.com>
2026-05-04 15:29:10 -0400

    Removed bad header, goes straight to about section now

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,12 @@ import AboutSection from '../components/about/AboutSection.jsx'
 import Contact from '../components/Contact.jsx'
 import Experience from '../components/Experience.jsx'
 import Footer from '../components/Footer.jsx'
-import Hero from '../components/Hero.jsx'
 import PortfolioSection from '../components/PortfolioSection.jsx'
 import Testimonials from '../components/Testimonials.jsx'
 
 export default function HomePage() {
   return (
     <div className="home-page">
-      <Hero />
       <AboutSection />
       <Experience />
       <PortfolioSection />
